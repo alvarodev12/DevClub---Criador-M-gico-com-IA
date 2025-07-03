c82c4f2d2fa107968b4462c6715f71f3eda64839
alvarodev12 <alsouza23@outlook.com>
2025-07-03 20:16:50 -0300

    feat: implementar click para pegar valor do input e desabilitar botão magic

---
+++ scritp.js
@@ -0,0 +1,7 @@
+function clickCreateAnimation() {
+  let inputAnimation = document.querySelector("input-animation").value;
+  let button = document.querySelector(".put-magic");
+  button.disabled = true;
+
+  console.log(inputAnimation);
+}
