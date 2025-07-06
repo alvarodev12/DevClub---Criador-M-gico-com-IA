f7b820d9d71f9d47240f8e6645a5e3c2c6fc3087
alvarodev12 <alsouza23@outlook.com>
2025-07-05 23:19:07 -0300

    feat: renomeação nome da pasta do projeto, e configuração do n8n

--- scritp.js
+++ scritp.js
@@ -2,6 +2,8 @@ let webhook = "https://alvarodev12.app.n8n.cloud/webhook-test/animacao-css";
 
 async function cliqueiNoBotao() {
   let textoInput = document.querySelector(".input-animacao").value;
+  let codigo = document.querySelector(".area-codigo");
+  let areaResultado = document.querySelector(".area-resultado");
 
   let resposta = await fetch(webhook, {
     method: "POST",
@@ -13,5 +15,8 @@ async function cliqueiNoBotao() {
 
   let info = JSON.parse(resultado.resposta);
 
-  console.log(info);
+  codigo.innerHTML = info.code;
+  areaResultado.innerHTML = info.preview;
+
+  document.head.insertAdjacentHTML("beforeend", `<style>${info.style}</style>`);
 }
