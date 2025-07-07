893f02795dc850b222a44053990a2e98d8de5676
alvarodev12 <alsouza23@outlook.com>
2025-07-06 23:07:56 -0300

    feat: adicionada background no button ao recarregar

--- scritp.js
+++ scritp.js
@@ -5,6 +5,11 @@ async function cliqueiNoBotao() {
   let codigo = document.querySelector(".area-codigo");
   let areaResultado = document.querySelector(".area-resultado");
 
+  let button = document.querySelector(".botao-magico");
+  button.disabled = true;
+  button.innerHTML = "Criando...";
+  button.style.backgroundColor = "#ccc";
+
   let resposta = await fetch(webhook, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -19,6 +24,16 @@ async function cliqueiNoBotao() {
   areaResultado.innerHTML = info.preview;
 
   document.head.insertAdjacentHTML("beforeend", `<style>${info.style}</style>`);
+
+  setTimeout(() => {
+    button.disabled = false;
+    button.innerHTML = "Criar Mágica 🧙‍♂️";
+  });
+  button.style.backgroundColor = "#37E539";
 }
 
 // ("beforeend", "<style>"+ info.style +"</style>")
+
+// button.disabled = false;
+// button.innerHTML = "Criar Mágica 🧙‍♂️";
+// button.style.backgroundColor = "#37E539";
